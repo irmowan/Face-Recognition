f6d44c99222d799ef7c6395b1a2d9f88355d4270
irmowan <irmowan@gmail.com>
2017-04-27 15:07:24 +0800

    Use index as label

--- generate_list.py
+++ generate_list.py
@@ -10,11 +10,11 @@ OUTPUT_FILE = "list.txt"
 root_dir = "data/"
 
 if __name__ == "__main__":
-    if os.path.exists(OUTPUT_FILE):
+    if os.path.exists(root_dir):
         with open(OUTPUT_FILE, "w") as f:
             for _0, dir_names, _1 in os.walk(root_dir):
-                for label in dir_names:
+                for idx, label in enumerate(dir_names):
                     for _2, _3, images in os.walk(root_dir + label):
                         for image in images:
                             if image[-3:] == 'jpg':
-                                f.write(label + '/' + image + ' ' + label + '\n')
+                                f.write(label + '/' + image + ' ' + str(idx) + '\n')
