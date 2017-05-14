eb54733682b9099f6be79c23805f61ea418a6e4c
irmowan <irmowan@gmail.com>
2017-05-14 17:11:02 +0800

    Modify make tfrecords, with label replace

--- make_tfrecords.py
+++ make_tfrecords.py
@@ -17,18 +17,25 @@ if __name__ == "__main__":
     print('Size of the queue is ' + str(len(input_queue)))
     images = []
     labels = []
+    label_map = {}
     writer = tf.python_io.TFRecordWriter(OUTPUT_FILE)
     while len(input_queue) > 0:
         image, label_idx, landmark = input.read_image_from_disk(input_queue)
+        if label_idx in label_map.keys():
+            new_label = label_map[label_idx]
+        else:
+            new_label = len(label_map)
+            label_map[label_idx] = new_label
         if image.ndim == 2:
             image = np.dstack([image] * 3)
         cropped_image = transform.img_process(image, landmark)
         image_raw = cropped_image.tostring()
         example = tf.train.Example(features=tf.train.Features(feature={
-            'label': tf.train.Feature(int64_list=tf.train.Int64List(value=[label_idx])),
+            'label': tf.train.Feature(int64_list=tf.train.Int64List(value=[new_label])),
             'image_raw': tf.train.Feature(bytes_list=tf.train.BytesList(value=[image_raw]))
         }))
         writer.write(example.SerializeToString())
         if len(input_queue) % 1000 == 0:
             print('Left %d images to pack' % len(input_queue))
+    print(label_map)
     writer.close()
