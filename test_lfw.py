d2c1b2412c0a4a56fe5d65024132dbc63f4dd995
irmowan <irmowan@gmail.com>
2017-05-17 16:29:24 +0800

    Complete test lfw, with vgg_16

--- test_lfw.py
+++ test_lfw.py
@@ -7,18 +7,22 @@ import tensorflow as tf
 import numpy as np
 import cv2
 import os.path
+import time
 import transform
 from tensorflow.contrib.slim.nets import vgg
+from numpy import dot
+from numpy.linalg import norm
 
 slim = tf.contrib.slim
 
 tf.app.flags.DEFINE_string('model_name', '', """""")
 tf.app.flags.DEFINE_string('lfw_data_dir', 'lfw_funneled', """""")
 tf.app.flags.DEFINE_string('model_dir', 'train_data/casia_train/', """""")
+tf.app.flags.DEFINE_integer('num_classes', 10575, """""")
 
 FLAGS = tf.app.flags.FLAGS
 
-threshold = 0.5
+threshold = 0.92
 size = 6000
 data_dir = 'dataset/lfw_funneled/'
 image_output_dir = 'images/lfw_align/'
@@ -26,120 +30,133 @@ lfw_landmark_file = 'txt/lfw_landmark.txt'
 pair_list_file = 'txt/pairs.txt'
 
 restore_model = 'model.ckpt'
-restore_step = 120000
+restore_step = 131000
 restore_file = restore_model + '-' + str(restore_step)
 
-no_landmark = 0
+extract_feature = 'vgg_16/fc6'
 
+class TestLFW():
+    def __init__(self):
+        self.dic = {}
+        self.sess = tf.Session()
+        self.end_points = None
+        self.images = tf.placeholder(tf.float32, shape=(None, 224, 224, 3))
+        self.dis_list = [] 
+        
+    def load_lfw_landmark(self):
+        with open(lfw_landmark_file, 'r') as f:
+            for line in f:
+                info = line.split()
+                filename = info[0]
+                self.dic[filename] = [int(x) for x in info[-10:]]
 
-def load_lfw_landmark():
-    dic = {}
-    with open(lfw_landmark_file, 'r') as f:
-        for line in f:
-            info = line.split()
-            filename = info[0]
-            dic[filename] = [int(x) for x in info[-10:]]
-    return dic
+    def def_net(self):
+        with slim.arg_scope(vgg.vgg_arg_scope()):
+            logits, end_points = vgg.vgg_16(self.images, num_classes=FLAGS.num_classes, 
+                                            dropout_keep_prob=1.0, is_training=False)
+        self.end_points = end_points
 
+    def restore_model(self):
+        saver = tf.train.Saver()
+        saver.restore(self.sess, FLAGS.model_dir + restore_file)
+        self.sess.run(tf.global_variables_initializer())
 
-def test_forward(image):
-    with slim.arg_scope(vgg.vgg_arg_scope()):
-        logits, end_points = vgg.vgg_16(image, num_classes=FLAGS.num_classes)
-    extract_feature = 'vgg16/fc7'
-    with tf.Session() as sess:
-        saver = tf.train.saver(tf.global_variables())
-        saver.restore(sess, restore_file)
-        sess.run(tf.global_variables_initializer())
-        end_point = sess.run([end_points])[0]
-        print(end_point.shape)
-        print(end_point)
+    def get_features(self, image_0, image_1):
+        images_0 = np.expand_dims(image_0, axis=0)
+        images_1 = np.expand_dims(image_1, axis=0)
+        images = np.concatenate((images_0, images_1), axis=0)
+        end_point = self.sess.run([self.end_points], feed_dict={self.images:images})[0]
+        features = end_point[extract_feature]
+        feature_0, feature_1 = features[0][0][0], features[1][0][0]
+        return feature_0, feature_1
 
+    def test_one_pair(self, image_file_pair):
+        image_file_0, image_file_1 = image_file_pair
+        image_0 = cv2.imread(data_dir + image_file_0)
+        image_1 = cv2.imread(data_dir + image_file_1)
 
-def test_one_pair(image_file_pair, dic):
-    image_file_0, image_file_1 = image_file_pair
-    image_0 = cv2.imread(data_dir + image_file_0)
-    image_1 = cv2.imread(data_dir + image_file_1)
+        if image_file_0 in self.dic.keys():
+            landmark_0 = self.dic[image_file_0]
+        else:
+            landmark_0 = None
+        crop_image_0 = transform.img_process(image_0, landmark_0)
+        assert crop_image_0.shape == (224, 224, 3)
 
-    if image_file_0 in dic.keys():
-        landmark_0 = dic[image_file_0]
-    else:
-        landmark_0 = None
-    crop_image_0 = transform.img_process(image_0, landmark_0)
-    assert crop_image_0.shape == (224, 224, 3)
+        if image_file_1 in self.dic.keys():
+            landmark_1 = self.dic[image_file_1]
+        else:
+            landmark_1 = None
+        crop_image_1 = transform.img_process(image_1, landmark_1)
+        assert crop_image_1.shape == (224, 224, 3)
 
-    if image_file_1 in dic.keys():
-        landmark_1 = dic[image_file_1]
-    else:
-        landmark_1 = None
-    crop_image_1 = transform.img_process(image_1, landmark_1)
-    assert crop_image_1.shape == (224, 224, 3)
+        feature_0, feature_1 = self.get_features(crop_image_0, crop_image_1)
+        distance = dot(feature_0, feature_1) / (norm(feature_0) * norm(feature_1))
+        if distance > threshold:
+            answer = True
+        else:
+            answer = False
+        # cv2.imwrite(image_output_dir + image_file_0.split('/')[1], image_0)
+        # cv2.imwrite(image_output_dir + image_file_1.split('/')[1], image_1)
+        # cv2.imwrite(image_output_dir + image_file_0.split('/')[1][:-4] + '_crop.jpg', crop_image_0)
+        # cv2.imwrite(image_output_dir + image_file_1.split('/')[1][:-4] + '_crop.jpg', crop_image_1)
+        return answer, distance
 
-    test_forward(crop_image_0)
-
-    test_forward(crop_image_1)
-
-    # cv2.imwrite(image_output_dir + image_file_0.split('/')[1], image_0)
-    # cv2.imwrite(image_output_dir + image_file_1.split('/')[1], image_1)
-    # cv2.imwrite(image_output_dir + image_file_0.split('/')[1][:-4] + '_crop.jpg', crop_image_0)
-    # cv2.imwrite(image_output_dir + image_file_1.split('/')[1][:-4] + '_crop.jpg', crop_image_1)
-    # feature_pair = []
-    # for image in image_pair:
-    #     landmark = get_landmark(image)
-    #     image_align = align(image)
-    #     feature_pair.append(get_feature(image_align))
-    # d = cal_distance(feature_pair)
-    # if d > threshold:
-    #     return True
-    # else:
-    #     return False
-    return True
-
-
-def test():
-    dic = load_lfw_landmark()
-
-    with open(pair_list_file) as f:
+    def test(self):
+        self.load_lfw_landmark()
+        self.def_net()
+        self.restore_model()
         cnt = 0
-        cnt_correct = 0
-        cnt_false_positive = 0
-        cnt_false_negative = 0
-        for line in f:
-            info = line.split()
-            image_pair = []
-            same = None
-            if len(info) == 3:
-                same = True
-                first = '/'.join([info[0], info[0] + '_' + '%04d' % int(info[1]) + '.jpg'])
-                second = '/'.join([info[0], info[0] + '_' + '%04d' % int(info[2]) + '.jpg'])
-                image_pair = [first, second]
-            elif len(info) == 4:
-                same = False
-                first = '/'.join([info[0], info[0] + '_' + '%04d' % int(info[1]) + '.jpg'])
-                second = '/'.join([info[2], info[2] + '_' + '%04d' % int(info[3]) + '.jpg'])
-                image_pair = [first, second]
-            else:
-                print('Line in the list error:')
-                print(line)
-                continue
-            answer = test_one_pair(image_pair, dic)
-            cnt += 1
-            # if (cnt == 10):
-            #    break
-            if answer == same:
-                cnt_correct += 1
-            elif answer and not same:
-                cnt_false_positive += 1
-            elif not answer and same:
-                cnt_false_negative += 1
-            # TODO: test only one case first
-            break
+        cnt_t_t = 0
+        cnt_t_f = 0
+        cnt_f_t = 0 
+        cnt_f_f = 0
+        start_time = time.time()
+        with open(pair_list_file) as f:
+            for line in f:
+                info = line.split()
+                image_pair = []
+                same = None
+                if len(info) == 3:
+                    same = True
+                    first = '/'.join([info[0], info[0] + '_' + '%04d' % int(info[1]) + '.jpg'])
+                    second = '/'.join([info[0], info[0] + '_' + '%04d' % int(info[2]) + '.jpg'])
+                    image_pair = [first, second]
+                elif len(info) == 4:
+                    same = False
+                    first = '/'.join([info[0], info[0] + '_' + '%04d' % int(info[1]) + '.jpg'])
+                    second = '/'.join([info[2], info[2] + '_' + '%04d' % int(info[3]) + '.jpg'])
+                    image_pair = [first, second]
+                else:
+                    print('Line in the list error:')
+                    print(line)
+                    continue
+                answer, distance = self.test_one_pair(image_pair)
+                print(same, answer, distance)
+                cnt += 1
+                if answer and same:
+                    cnt_t_t += 1
+                elif not answer and not same:
+                    cnt_f_f += 1
+                elif answer and not same:
+                    cnt_f_t += 1
+                elif not answer and same:
+                    cnt_t_f += 1
+                self.dis_list.append(distance)
+                if cnt % 500 == 0:
+                    print('Test %4d/%4d pairs' % (cnt, size))
+        duration = int(time.time() - start_time)
         assert cnt == size
-        print('Test completed.')
+        cnt_correct = cnt_t_t + cnt_f_f
+        print('Test completed, use %d seconds.' % duration)
         print('Count = %d' % cnt)
-        print('Correct   = %d, rate = %s' % (cnt_correct, format(cnt_correct / float(cnt), '6.2%')))
-        print('F-p count = %d, rate = %s' % (cnt_false_positive, format(cnt_false_positive / float(cnt), '6.2%')))
-        print('F-n count = %d, rate = %s' % (cnt_false_negative, format(cnt_false_negative / float(cnt), '6.2%')))
-
+        print('Correct = %d, rate = %s' % (cnt_correct, format(cnt_correct / float(cnt), '6.2%')))
+        print('True,  guess True  = %d, rate = %s' % (cnt_t_t, format(cnt_t_t / float(cnt), '6.2%')))
+        print('False, guess False = %d, rate = %s' % (cnt_f_f, format(cnt_f_f / float(cnt), '6.2%')))
+        print('Flase, guess True  = %d, rate = %s' % (cnt_f_t, format(cnt_f_t / float(cnt), '6.2%')))
+        print('True,  guess False = %d, rate = %s' % (cnt_t_f, format(cnt_t_f / float(cnt), '6.2%')))
+        self.dis_list.sort()
+        print('Medium distance: %.4f' % self.dis_list[cnt/2])
 
 if __name__ == "__main__":
-    test()
+    t = TestLFW()
+    t.test()
