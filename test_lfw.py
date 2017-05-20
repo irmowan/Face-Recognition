837d890b81bc3f01752606d07afdbb77a9390be7
irmowan <irmowan@gmail.com>
2017-05-20 11:38:34 +0800

    Add features store to test

--- test_lfw.py
+++ test_lfw.py
@@ -8,33 +8,39 @@ import numpy as np
 import cv2
 import os.path
 import time
+import pickle
 import transform
-from tensorflow.contrib.slim.nets import vgg
+from tensorflow.contrib.slim.nets import vgg, resnet_v1
 from numpy import dot
 from numpy.linalg import norm
 
 slim = tf.contrib.slim
 
-tf.app.flags.DEFINE_string('model_name', '', """""")
-tf.app.flags.DEFINE_string('lfw_data_dir', 'lfw_funneled', """""")
-tf.app.flags.DEFINE_string('model_dir', 'train_data/casia_train/', """""")
+tf.app.flags.DEFINE_string('lfw_data_dir', 'dataset/lfw_funneled/', """""")
 tf.app.flags.DEFINE_integer('num_classes', 10575, """""")
 
 FLAGS = tf.app.flags.FLAGS
 
-threshold = 0.35
+model_name = 'vgg_16'
+restore_model = 'model.ckpt'
+restore_step = 135000
+choose_feature = 'fc7'
+
 size = 6000
-data_dir = 'dataset/lfw_funneled/'
 image_output_dir = 'images/lfw_align/'
 lfw_landmark_file = 'txt/lfw_landmark.txt'
 pair_list_file = 'txt/pairs.txt'
-
-restore_model = 'model.ckpt'
-restore_step = 131000
+model_dir = 'train_data/casia_' + model_name + '/'
 restore_file = restore_model + '-' + str(restore_step)
-
-extract_feature = 'vgg_16/fc7'
-
+features_dict = {
+    'fc6': 'vgg_16/fc6',
+    'fc7': 'vgg_16/fc7',
+    'fc8': 'vgg_16/fc8',
+    'res': 'resnet_v1_101/block2/unit_2/bottleneck_v1/conv3'
+}
+extract_feature = features_dict[choose_feature]
+features_dir = 'features'
+output_feature_path = os.path.join(features_dir,'_'.join(('lfw', model_name, 'step' + str(restore_step), choose_feature)) + '.txt')
 
 class TestLFW():
     def __init__(self):
@@ -51,15 +57,19 @@ class TestLFW():
                 self.dic[filename] = [int(x) for x in info[-10:]]
 
     def def_net(self):
-        with slim.arg_scope(vgg.vgg_arg_scope()):
-            _, end_points = vgg.vgg_16(self.images, num_classes=FLAGS.num_classes,
-                                       dropout_keep_prob=1.0, is_training=False)
+        if model_name == 'vgg_16':
+            with slim.arg_scope(vgg.vgg_arg_scope()):
+                _, end_points = vgg.vgg_16(self.images, num_classes=FLAGS.num_classes,
+                                           dropout_keep_prob=1.0, is_training=False)
+        elif model_name == 'resnet_v1':
+            with slim.arg_scope(resnet_v1.resnet_arg_scope()):
+                _, end_points = resnet_v1.resnet_v1_101(self.images, num_classes=FLAGS.num_classes)
         self.end_points = end_points
 
     def restore_model(self):
         self.sess.run(tf.global_variables_initializer())
         saver = tf.train.Saver()
-        saver.restore(self.sess, FLAGS.model_dir + restore_file)
+        saver.restore(self.sess, model_dir + restore_file)
 
     def get_features(self, image_0, image_1):
         images_0 = np.expand_dims(image_0, axis=0)
@@ -72,8 +82,10 @@ class TestLFW():
 
     def test_one_pair(self, image_file_pair):
         image_file_0, image_file_1 = image_file_pair
-        image_0 = cv2.imread(data_dir + image_file_0)
-        image_1 = cv2.imread(data_dir + image_file_1)
+        image_0 = cv2.imread(FLAGS.lfw_data_dir + image_file_0)
+        image_1 = cv2.imread(FLAGS.lfw_data_dir + image_file_1)
+        assert image_0 is not None
+        assert image_1 is not None
         if image_file_0 in self.dic.keys():
             landmark_0 = self.dic[image_file_0]
         else:
@@ -88,7 +100,8 @@ class TestLFW():
         assert crop_image_1.shape == (224, 224, 3)
 
         feature_0, feature_1 = self.get_features(crop_image_0, crop_image_1)
-
+        assert feature_0.ndim == 1
+        assert feature_1.ndim == 1
         # feature_0 = feature_0 / norm(feature_0)
         # feature_1 = feature_1 / norm(feature_1)
         # similarity = norm(feature_0-feature_1)
@@ -153,10 +166,12 @@ class TestLFW():
 
         start_time = time.time()
         print('Begin test...')
-        for image_pair in image_pairs:
+        for idx, image_pair in enumerate(image_pairs):
             feature_0, feature_1, similarity = self.test_one_pair(image_pair['files'])
             image_pair['features'] = [feature_0, feature_1]
             image_pair['similarity'] = similarity
+            if (idx + 1) % 500 == 0:
+                print('Tested %4d/%4d pairs' % (idx + 1, size))
             
         duration = int(time.time() - start_time)
         print('Test completed, use %d seconds.' % duration)
@@ -167,11 +182,15 @@ class TestLFW():
         best_correct, best_threshold, best_t_t, best_f_f = self.search_threshold(sorted_pairs)
         print('Choose threshold: %.4f' % best_threshold)
 
-        print('Size = %d, Correct = %d, rate = %s' % (size, best_correct, format(best_correct / float(size), '6.2%')))
-        print('True,  guess True  = %d, rate = %s' % (best_t_t, format(best_t_t / float(size), '6.2%')))
-        print('False, guess False = %d, rate = %s' % (best_f_f, format(best_f_f / float(size), '6.2%')))
-
+        print('Size = %d, Correct = %4d, rate = %s' % (size, best_correct, format(best_correct / float(size), '6.2%')))
+        print('True,  guess True  = %4d, rate = %s' % (best_t_t, format(best_t_t / float(size), '6.2%')))
+        print('False, guess False = %4d, rate = %s' % (best_f_f, format(best_f_f / float(size), '6.2%')))
+        print('Storing features...')
+        pickle.dump(image_pairs, open(output_feature_path, 'w'))
+        print('Features stored at %s' % output_feature_path)
 
 if __name__ == "__main__":
+    if not tf.gfile.Exists(features_dir):
+        tf.gfile.MakeDirs(features_dir)
     t = TestLFW()
     t.test()
